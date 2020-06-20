3e29054c6eb557ac06a2dc0349374918890e9857
sabinasunuwar <sabinasunuwar4@gmail.com>
2020-06-20 21:20:25 +0545

    Register Form Test

---
+++ tests/Unit/Auth/RegisterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+
+    public function register_displays_the_register_form()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.register');
+    }
+
+    /** @test */
+
+    public function user_cannot_view_a_register_form_when_authenticated()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/register');
+
+        $response->assertRedirect('/home');
+    }
+
+}
