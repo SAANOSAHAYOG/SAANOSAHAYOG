36f3cbd6e6a06970b0a3ee829cff7d0a4f25efb8
SudarsanUdash <sudremotae@gmail.com>
2020-06-22 12:44:57 +0545

    Login Test Completed

---
+++ tests/Unit/Auth/LoginTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Unit\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+
+    public function login_displays_the_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    /** @test */
+
+    public function user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/home');
+    }
+
+    /** @test */
+
+    public function user_can_login_with_correct_credentials()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+
+    }
+
+    /** @test */
+
+    public function user_cannot_login_with_invalid_email()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'nobody@example.com',
+            'password' => bcrypt($password = 'secret')
+        ]);
+
+        $response = $this->from('/login')->post('/login', [
+            'email' => 'invalid-email',
+            'password' => 'secret',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    /** @test */
+
+    public function user_cannot_login_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'secret'),
+        ]);
+
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    /** @test */
+
+    public function authenticated_user_can_logout()
+    {
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertRedirect('/');
+        $this->assertGuest();
+    }
+}
