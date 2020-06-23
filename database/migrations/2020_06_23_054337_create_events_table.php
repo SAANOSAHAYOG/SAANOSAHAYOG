3230c981647801963da962229a52fa0054929c2d
maharjansumina <suminamaharjan098@gmail.com>
2020-06-23 11:39:03 +0545

    Event table Added.

---
+++ database/migrations/2020_06_23_054337_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('location');
+            $table->string('startdate');
+            $table->string('starttime');
+            $table->string('enddate');
+            $table->string('endtime');
+            $table->string('eventname')->unique();
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
