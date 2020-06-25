dd884548c9415f3dbd8f90441b1f17078bcdca97
deem4sters <developerdeem4sters@gmail.com>
2020-06-25 20:03:45 +0545

    About Us Frontend Added

--- resources/views/saanosahayog/about-us.blade.php
+++ resources/views/saanosahayog/about-us.blade.php
@@ -3,10 +3,12 @@
 @section('content')
 <section id="call-to-action" class="wow fadeIn">
 	<div class="container text-center">
-		<h1>Event</h1>
-		<p>It's not what you achieve, it's what you overcome.</p>
+		<h1>About Us</h1>
+		<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Facilis alias dicta non ratione voluptate, quae amet deleniti magnam eius architecto eos nam officiis ipsa temporibus reiciendis cupiditate blanditiis culpa necessitatibus.</p>
 	</div>
 </section>
+
+
 
 
 @endsection
