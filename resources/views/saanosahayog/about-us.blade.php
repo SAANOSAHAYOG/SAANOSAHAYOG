3ff554d1573e317d890bf521e6ba5069d2eedd0f
deem4sters <developerdeem4sters@gmail.com>
2020-07-08 13:47:03 +0545

    About-us design added

--- resources/views/saanosahayog/about-us.blade.php
+++ resources/views/saanosahayog/about-us.blade.php
@@ -1,250 +1,93 @@
 @extends('saanosahayog.main')
-
 @section('content')
-<section id="call-to-action" class="wow fadeIn">
-	<div class="container text-center">
-		<h1>About Us</h1>
-		<p></p>
-	</div>
-</section>
-<section class="story-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
-   <div class="container text-center">
-      <h2>
-         WHAT DRIVES US
-      </h2>
-      <h3>
-         Together with Team
-      </h3>
-      <div class="col-md-8 col-md-offset-2">
-         <div class="red-border">
-            &nbsp;
-         </div>
-         <p class="ct-u-size22 ct-u-fontWeight300 marginTop40">
-            Sano Sahayog is a HELPFORUM for millions of children across Nepal. It is a 24-hour a day, 365 days a year, free, service for children in need of aid and assistance. We not only respond to the emergency needs of children but also link them to relevant services for their long-term care and rehabilitation. We have, till date, connected to three million children across the nation offering them care and protection.
 
-            It is also a agency of the  Women and Child Development acting as the parent organisation for setting up, managing and monitoring the service all over the country. Responsible for establishing the CHILDLINE service across the country, monitoring of service delivery and finance, training, research and documentation, creating awareness, advocacy as well as resource generation for the service.
-         </p>
-         <!-- <a class="ct-u-marginTop60 btn btn-solodev-red btn-fullWidth-sm ct-u-size19" href="#">Learn More</a> -->
-      </div>
-   </div>
-</section>
-<section class="culture-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
-   <div class="container">
-      <div class="col-md-8 col-md-offset-2">
-         <h2>
-           Together WE 
-         </h2>
-         <h3>
-            Help and Donations
-         </h3>
-         <p class="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60">
-            Sano Sahayog Guided by the aspirations of affected people, we pursue our mission by helping children and women <br>
-            We believe that for a helpline to be successful and effective in reaching out to the millions of children deprived of their childhood, we need to work in a partnership framework.. 
-         </p>
-      </div>
-      <div class="row ct-u-paddingBoth20">
-         <div class="col-xs-6 col-md-4">
-            <div class="company-icons-white">
-               <i class="fa fa-medkit" aria-hidden="true"></i>
-               <p>
-                  First Aids 
-               </p>
-               <p class="company-icons-subtext hidden-xs">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-               </p>
-            </div>
-         </div>
-         <div class="col-xs-6 col-md-4">
-            <div class="company-icons-white">
-               <i class="fa fa-money" aria-hidden="true"></i>
-               <p>
-                  Donations
-               </p>
-               <p class="company-icons-subtext hidden-xs">
-                  Praesent sed libero vel ex maximus vulputate nec eu ligula.
-               </p>
-            </div>
-         </div>
-         <div class="col-xs-6 col-md-4">
-            <div class="company-icons-white">
-               <i class="fa fa-clock-o" aria-hidden="true"></i>
-               <p>
-                  Time Management
-               </p>
-               <p class="company-icons-subtext hidden-xs">
-                  Vestibulum elementum nisi ut fermentum lobortis. 
-               </p>
+   <section id="call-to-action" class="wow fadeIn">
+      <div class="container text-center">
+         <div class="breadcrumb-text">
+            <h2> About Us </h2>
+            <div class="bt-option">
+               <a href="#">Home</a>
+               <span>About</span>
             </div>
          </div>
       </div>
-      <div class="row ct-u-paddingBoth20">
-         <div class="col-xs-6 col-md-4">
-            <div class="company-icons-white">
-               <i class="fa fa-coffee" aria-hidden="true"></i>
-               <p>
-                  SEMPER IPSUM
-               </p>
-               <p class="company-icons-subtext hidden-xs">
-                  Nullam bibendum felis non laoreet rutrum.
-               </p>
+   </section>
+<br>
+   <section class="about-section spad">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-6">
+               <div class="about-pic">
+                  <img src="{{ url('public/saanosahayog/img/about.jpg') }}" alt="">
+               </div>
             </div>
-         </div>
-         <div class="col-xs-6 col-md-4">
-            <div class="company-icons-white">
-               <i class="fa fa-gamepad" aria-hidden="true"></i>
-               <p>
-                  Games
-               </p>
-               <p class="company-icons-subtext hidden-xs">
-                  Etiam diam mi, lacinia eu sapien in, dapibus sodales erat.
-               </p>
-            </div>
-         </div>
-         <div class="col-xs-6 col-md-4">
-            <div class="company-icons-white">
-               <i class="fa fa-cutlery" aria-hidden="true"></i>
-               <p>
-                  Foods
-               </p>
-               <p class="company-icons-subtext hidden-xs">
-                  Etiam varius porttitor tellus et aliquet.
-               </p>
+            <div class="col-lg-6">
+               <div class="about-text">
+                  <h3>The 2019 Conference</h3>
+                  <p>When I first got into the online advertising business, I was looking for the magical combination that would put my website into the top search engine rankings, catapult me to the forefront of the minds or individuals looking to buy my product, and generally make me rich beyond my wildest dreams! After succeeding in the business for this long, I’m able to look back on my old self with this kind of thinking and shake my head. </p>
+                  <ul>
+                     <li><span class="icon_check"></span> Write On Your Business Card</li>
+                     <li><span class="icon_check"></span> Advertising Outdoors</li>
+                     <li><span class="icon_check"></span> Effective Advertising Pointers</li>
+                     <li><span class="icon_check"></span> Kook 2 Directory Add Url Free</li>
+                  </ul>
+               </div>
             </div>
          </div>
       </div>
-      <a class="ct-u-marginTop60 btn btn-solodev-red-reversed btn-fullWidth-sm ct-u-size19" href="/">Ready to Learn More?</a>
-   </div>
-</section>
-<section class="customers-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
-<div class="container">
-   <div class="col-md-8 col-md-offset-2">
-      <h2>
-         Our Affiliations and Donars 
-      </h2>
-      <h3>
-         Trusted by some of the world&rsquo;s leading brands.
-      </h3>
-      <p class="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60 marginTop40">
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed libero vel ex maximus vulputate nec eu ligula. Vestibulum elementum nisi ut fermentum lobortis. Sed quis iaculis felis. 
-      </p>
-   </div>
-   <div class="clearfix">
-      &nbsp;
-   </div>
-</div>
-</header>
-<main>
-   <div class="container ct-u-paddingTop40 ct-u-paddingBottom100">
-      <div class="row">
-         <div class="col-md-12 ct-content">
-            <section class="clients-home">
-               <div class="container">
-                  <div class="clients-logos text-center">
-                     <!-- starting row div -->
-                     <div class="row">
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/zeina.html" class="Zeina"><img alt="images/logo-zeina.png" src="images/logo-zeina.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Zeina - 0
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/logic.html" class="Logic"><img alt="images/logic.png" src="images/logic.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Logic
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/smart.html" class="Smart"><img src="images/client3.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Smart 
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+   </section>
+   <!-- About Section End -->
+   <!-- Story Section Begin -->
+   <section class="story-section spad">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-12">
+               <div class="section-title">
+                  <h2>Our Story</h2>
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-lg-6">
+               <div class="story-left">
+                  <div class="story-item">
+                     <h2>2008</h2>
+                     <div class="si-text">
+                        <h4>Adwords Keyword Research For Beginners</h4>
+                        <p>However this is also the most expensive position. Give it a try if the second to fourth display position gives you more visitors and more customers for less money.</p>
                      </div>
-                     <div class="row">
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/softtech.html" class="Softtech"><img alt="images/softtech.png" src="images/softtech.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Softtech 
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/wheel.html" class="Wheel"><img alt="images/logo-target.png" src="images/logo-target.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Wheel 
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/3designs.html" class="3designs"><img alt="images/designx.png" src="images/designx.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    3designs 
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
+                  </div>
+                  <div class="story-item">
+                     <h2>2011</h2>
+                     <div class="si-text">
+                        <h4>Adwords Keyword Research For Beginners</h4>
+                        <p>Virgin Mobile took a more effective approach in marketing their cell phone service by focusing not on the people that would be making the actual purchase.</p>
                      </div>
-                     <div class="row">
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/heart.html" class="Heart"><img alt="images/client7.png" src="images/client7.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Heart
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/devtech.html" class="Devtech"><img alt="images/devtech.png" src="images/devtech.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Devtech 
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <div class="col-md-4 client-logos-repeater">
-                           <a href="/path/to/detail/chartz.html" class="Chartz"><img alt="images/chartz.png" src="images/chartz.png"></a>
-                           <div class="logo-title">
-                              <div class="displayTable">
-                                 <div class="displayTableCell">
-                                    Chartz 
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <!-- closing row div -->
                   </div>
                </div>
-            </section>
+            </div>
+            <div class="col-lg-6">
+               <div class="story-right">
+                  <div class="story-item">
+                     <h2>2015</h2>
+                     <div class="si-text">
+                        <h4>15 Tips To Increase Your Adwords Profits</h4>
+                        <p>There is no better advertisement campaign that is low cost and also successful at the same time. Great business ideas when utilized effectively can save lots of money.</p>
+                     </div>
+                  </div>
+                  <div class="story-item">
+                     <h2>2019</h2>
+                     <div class="si-text">
+                        <h4>Advertising Internet Online Opportunities To Explore</h4>
+                        <p>Many people sign up for affiliate programs with the hopes of making some serious money. They advertise a few places and then wait for the money to start pouring in.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
          </div>
-     </div>
-
-
-
-
+      </div>
+   </section>
 @endsection
+Chat conversation end
+Type a message...
+
