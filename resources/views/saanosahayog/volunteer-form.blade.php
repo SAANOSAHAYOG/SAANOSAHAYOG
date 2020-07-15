0dceb98a1ee6557a3d1b763bad2b70b7d9e57563
deem4sters <developerdeem4sters@gmail.com>
2020-07-15 19:02:39 +0545

    Voluteer form view page added

---
+++ resources/views/saanosahayog/volunteer-form.blade.php
@@ -0,0 +1,70 @@
+@extends('saanosahayog.main')
+@section('content')
+
+    <section id="call-to-action" class="wow fadeIn">
+        <div class="container text-center">
+            <div class="breadcrumb-text">
+                <h2>Volunteer Form</h2>
+                <div class="bt-option">
+                    <a href="#">Volunteer</a>
+                    <span>Volunteer Form</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="contact-from-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Registration for Volunteer</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form action="" method="POST" class="comment-form contact-form" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <label>Full Name</label>
+                                <input type="text" name="name" placeholder="Name">
+                            </div>
+                            <div class="col-lg-4">
+                                <label>Email</label>
+                                <input type="text" name="email" placeholder="Email">
+                            </div>
+                            <div class="col-lg-4">
+                                <label>Phone</label>
+                                <input type="text" name="phone" placeholder="Phone">
+                            </div>
+                            <div class="col-lg-12">
+                                <label>Address</label>
+                                <input type="text" name="address" placeholder="Address">
+                            </div>
+                            <div class="col-lg-4">
+                                <label>City</label>
+                                <input type="text" name="city" placeholder="City">
+                            </div>
+                            <div class="col-lg-4">
+                                <label>State</label>
+                                <input type="text" name="state" placeholder="State">
+                            </div>
+                            <div class="col-lg-4">
+                                <label>Zip Code</label>
+                                <input type="text" name="zipcode" placeholder="Zip Code">
+                            </div>
+                            <div class="col-lg-12">
+                                <label>How did you learn about SAANOSAHAYOG?</label>
+                                <textarea name="learn" maxlength="500" rows="10" cols="30"></textarea>
+                            </div>
+                            <div class="col-lg-12 text-center">
+                                <button type="submit" class="site-btn">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    @endsection
