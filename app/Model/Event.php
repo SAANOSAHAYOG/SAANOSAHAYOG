92f18ae7c2f6299f60f1473fe9bf592ff5b3fe82
sabinasunuwar <sabinasunuwar4@gmail.com>
2020-07-24 17:54:54 +0545

    Changes in Blog & Event Model.

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -3,8 +3,22 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Searchable\Searchable;
+use Spatie\Searchable\SearchResult;
 
-class Event extends Model
+class Event extends Model implements Searchable
 {
     protected $table = 'events';
+    protected $fillable = ['location'];
+
+    public function getSearchResult(): SearchResult
+    {
+        $url = route('event', $this->id);
+
+        return new SearchResult(
+            $this,
+            $this->location,
+            $url
+         );
+    }
 }
